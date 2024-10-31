0388b7347a44839a0ab325e4434266151e245001
Luiz166 <otavio.tata.morais@gmail.com>
2024-10-31 16:24:36 -0300

    add navbar, add main section

--- app/page.js
+++ app/page.js
@@ -1,101 +1,56 @@
 import Image from "next/image";
+import { IoIosSearch } from "react-icons/io"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div>
+      <nav className="flex items-center justify-between px-5">
+        <div className="flex items-center">
+          <Image
+            src="/logo.png"
+            width={100}
+            height={100}
+            alt="Logo"
+          />
+          <span className="font-bold text-4xl">Recify</span>
+        </div>
+        <div className="flex space-x-10">
+          <a className="text-gray-600" href="#">Recipes</a>
+          <a className="text-gray-600" href="#">About</a>
+          <a className="text-gray-600" href="#">Contact</a>
+        </div>
+        <div className="flex items-center">
+          <button type="button" className="rounded-full bg-gray-300 p-2">
+            <IoIosSearch size={25}/>
+          </button>
+        </div>
+      </nav>
+      <main className="mt-10 flex items-center justify-center space-x-5">
+        <div className="flex flex-col items-center justify-center w-96 space-y-5">
+          <span className="text-[#a7cd3a] font-bold text-3xl">
+            Cooking 
+            <span className="text-black"> with fun made easy and accessible</span>
+          </span>
+          <span className="text-sm text-gray-600">
+            It's easy to use and offers a variety of irresistable recipes that
+            appeal to beginners and experts alike. Ready to get cooking?
+          </span>
+          <div className="flex space-x-5">
+            <button type="button" 
+            className="bg-[#b1f8f2] text-white rounded-lg p-2 font-bold text-2xl">Get started</button>
+            <button type="button"
+            className="bg-[#d5bbb1] text-white rounded-lg p-2 font-bold text-2xl">Learn more</button>
+          </div>
+        </div>
+        <div className="overflow-hidden rounded-r-2xl">
+          <Image
+            src="/food-pexelsimg.jpg"
+            width={500}
+            height={500}
+            alt="food-pic"
+          />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
