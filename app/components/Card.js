5aef09114af2b4f4325b51cd3c486059f3a33417
Luiz166 <otavio.tata.morais@gmail.com>
2024-11-01 12:05:49 -0300

    styling title, calories div, card div

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,13 +1,13 @@
 export default function Card(data){
     return(
-        <div className="card border p-2 rounded-lg shadow-md flex flex-col">
+        <div className="card border p-2 rounded-lg shadow-md items-center flex flex-col">
             <div className="w-[300px] h-[300px]">
                 <img className="h-full w-full object-cover" src={data.data.recipe.images.REGULAR.url} alt={data.data.recipe.label}/>
             </div>
-            <span className="text-nowrap">
+            <span className="text-nowrap text-lg font-semibold">
                 {data.data.recipe.label}
             </span>
-            <div className="flex justify-between">
+            <div className="flex justify-between w-full">
                 <span>
                     Calories:
                 </span>
