91fba1fa1d5a91807e5a10ff64d5a1388461223a
Luiz166 <otavio.tata.morais@gmail.com>
2024-11-02 19:01:49 -0300

    add link to card

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,20 +1,24 @@
 export default function Card(data){
+    const recipeUrl = data.data._links.self.href
+    const recipeId = recipeUrl.split('/')[6].split('?')[0];
     return(
-        <div className="card border p-2 rounded-lg shadow-md items-center flex flex-col">
-            <div className="w-[300px] h-[300px]">
-                <img className="h-full w-full object-cover" src={data.data.recipe.images.REGULAR.url} alt={data.data.recipe.label}/>
-            </div>
-            <span className="text-nowrap text-lg font-semibold">
-                {data.data.recipe.label}
-            </span>
-            <div className="flex justify-between w-full">
-                <span>
-                    Calories:
+        <a href={`/recipes/${recipeId}`}>
+            <div className="card border p-2 rounded-lg shadow-md items-center flex flex-col">
+                <div className="w-[300px] h-[300px]">
+                    <img className="h-full w-full object-cover" src={data.data.recipe.images.REGULAR.url} alt={data.data.recipe.label}/>
+                </div>
+                <span className="text-nowrap text-lg font-semibold">
+                    {data.data.recipe.label}
                 </span>
-                <span className="text-[#a7cd3a]">
-                    {Math.round(data.data.recipe.calories)}
-                </span>
+                <div className="flex justify-between w-full">
+                    <span>
+                        Calories:
+                    </span>
+                    <span className="text-[#a7cd3a]">
+                        {Math.round(data.data.recipe.calories)}
+                    </span>
+                </div>
             </div>
-        </div>
+        </a>
     )
 }
